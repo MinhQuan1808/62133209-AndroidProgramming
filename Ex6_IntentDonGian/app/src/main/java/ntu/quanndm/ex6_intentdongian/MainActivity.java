be1fcd7a1b0ca0077a5a8295e664407c489f5a8b
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-07 12:51:41 +0700

    Cap nhat Ex6 lan 2

--- Ex6_IntentDonGian/app/src/main/java/ntu/quanndm/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/quanndm/ex6_intentdongian/MainActivity.java
@@ -3,12 +3,19 @@ package ntu.quanndm.ex6_intentdongian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnMH2, btnMH3;
+    void TimDieuKhien(){
+        btnMH2 = findViewById(R.id.btnMH2);
+        btnMH3 = findViewById(R.id.btnMH3);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
     }
 }
