075f1cc6ddf730f407a1958bfa253c38c91b1f34
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-07 14:10:22 +0700

    Hoan thanh Ex7_IntentLogin, test OK

--- Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/HomeActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/HomeActivity.java
@@ -2,7 +2,9 @@ package ntu.quanndm.ex7_intentlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intentTuLogin = getIntent();
+        String tenDN_Nhan = intentTuLogin.getStringExtra("ten_dang_nhap");
+        TextView tvUserName = findViewById(R.id.tvUserName);
+        tvUserName.setText(tenDN_Nhan);
     }
 }
