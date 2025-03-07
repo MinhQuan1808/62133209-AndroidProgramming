131755798a330dfb607d9928180fcbcd12004ae9
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-07 13:56:52 +0700

    Cap nhat Ex7 lan 5

--- Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/LoginActivity.java
@@ -3,6 +3,8 @@ package ntu.quanndm.ex7_intentlogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnOK = findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
--- Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/MainActivity.java
@@ -2,7 +2,10 @@ package ntu.quanndm.ex7_intentlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
+        });
     }
 }
