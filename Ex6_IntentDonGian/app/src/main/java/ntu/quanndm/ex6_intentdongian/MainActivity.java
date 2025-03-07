393bf0a591d573d1a8ccfcdceb0d38b4af67c599
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-07 12:58:55 +0700

    Hoan thanh Ex6_IntentDonGian, test OK

--- Ex6_IntentDonGian/app/src/main/java/ntu/quanndm/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/quanndm/ex6_intentdongian/MainActivity.java
@@ -2,7 +2,9 @@ package ntu.quanndm.ex6_intentdongian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TimDieuKhien();
+        btnMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(intentMH2);
+            }
+        });
+        btnMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                startActivity(intentMH3);
+            }
+        });
     }
 }
