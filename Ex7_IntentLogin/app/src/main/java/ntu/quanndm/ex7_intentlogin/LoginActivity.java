a1db088708db3f8a791986600efd1e4bda22e68e
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-07 14:05:58 +0700

    Cap nhat Ex7 lan 6

--- Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/quanndm/ex7_intentlogin/LoginActivity.java
@@ -2,9 +2,12 @@ package ntu.quanndm.ex7_intentlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,7 +19,23 @@ public class LoginActivity extends AppCompatActivity {
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                EditText edtUserName = findViewById(R.id.edtUserName);
+                EditText edtPass = findViewById(R.id.edtPass);
+                EditText edtMail = findViewById(R.id.edtMail);
+                String tenDangNhap = edtUserName.getText().toString();
+                String mk = edtPass.getText().toString();
+                String mail = edtMail.getText().toString();
+                if (tenDangNhap.equals("nguyendinhminhquan") && mk.equals("62133209"))
+                {
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap", mk);
+                    startActivity(iQuiz);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Bạn nhập sai thông tin", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
