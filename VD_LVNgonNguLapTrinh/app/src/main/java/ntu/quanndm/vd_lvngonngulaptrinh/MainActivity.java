a534c7a7f827ad612759465f3b33da104d218c5b
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-13 05:58:17 +0700

    Cap nhat VD LV lan 1

--- VD_LVNgonNguLapTrinh/app/src/main/java/ntu/quanndm/vd_lvngonngulaptrinh/MainActivity.java
+++ VD_LVNgonNguLapTrinh/app/src/main/java/ntu/quanndm/vd_lvngonngulaptrinh/MainActivity.java
@@ -3,12 +3,37 @@ package ntu.quanndm.vd_lvngonngulaptrinh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView LVNNLT;
+    ArrayList<String> dsNgonNguLT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LVNNLT = findViewById(R.id.LVNgonNguLapTrinh);
+
+        //B1.Chuan bi du lieu, hard-core
+        dsNgonNguLT = new ArrayList<String>();
+        dsNgonNguLT.add("Python");
+        dsNgonNguLT.add("Php");
+        dsNgonNguLT.add(("Java"));
+        //B2
+        ArrayAdapter<String> adapterNNLT;
+        adapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, dsNgonNguLT);
+        //B3. Gan adapter
+        LVNNLT.setAdapter(adapterNNLT);
+        //B4. Gan bo lang nghe va xu su kien
+        LVNNLT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
