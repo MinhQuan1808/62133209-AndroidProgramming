17eb8dce8046df29bfb09ca1b9d6036d50615c48
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-25 07:32:20 +0700

    Hoan thanh, test OK

--- LamLai_Listview/app/src/main/java/ntu/quanndm/lamlai_listview/MainActivity.java
+++ LamLai_Listview/app/src/main/java/ntu/quanndm/lamlai_listview/MainActivity.java
@@ -3,12 +3,26 @@ package ntu.quanndm.lamlai_listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lvHoa;
+    ArrayList<String> dsHoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvHoa = findViewById(R.id.lvHoa);
+        dsHoa = new ArrayList<String>();
+        dsHoa.add("Hồng");
+        dsHoa.add("Đào");
+        dsHoa.add("Lan");
+        dsHoa.add("Cúc");
+        ArrayAdapter adapterHoa;
+        adapterHoa = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, dsHoa);
+        lvHoa.setAdapter(adapterHoa);
     }
 }
