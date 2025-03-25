56c38afbd7e9505efe19a40450a128c7ae5910aa
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-25 08:27:48 +0700

    Hoan thanh, test OK

--- LamLai_Onclick2/app/src/main/java/ntu/quanndm/lamlai_onclick2/MainActivity.java
+++ LamLai_Onclick2/app/src/main/java/ntu/quanndm/lamlai_onclick2/MainActivity.java
@@ -3,6 +3,10 @@ package ntu.quanndm.lamlai_onclick2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText edtTen = findViewById(R.id.edtTen);
+        Button btnChao = findViewById(R.id.btnChao);
+        btnChao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Xin chao" + edtTen.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
