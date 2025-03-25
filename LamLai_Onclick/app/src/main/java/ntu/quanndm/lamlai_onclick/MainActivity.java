9a751ec0d89ec17b819db603c7124d4eba5dcf72
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-25 07:40:09 +0700

    Hoan thanh, test OK

---
+++ LamLai_Onclick/app/src/main/java/ntu/quanndm/lamlai_onclick/MainActivity.java
@@ -0,0 +1,24 @@
+package ntu.quanndm.lamlai_onclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnChao = findViewById(R.id.btnChao);
+        btnChao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Xin chao", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
