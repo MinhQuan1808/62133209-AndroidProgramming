2744e8fa66e1006801805090e13601f2ede15b1e
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-31 09:26:30 +0700

    cap nhat lan 4

--- LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/Cau1Activity.java
+++ LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/Cau1Activity.java
@@ -6,14 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Cau1Activity extends AppCompatActivity {
-
+    EditText edtA, edtB, edtKQ;
+    Button btnTong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cau1);
-
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnTong = findViewById(R.id.btnTong);
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
