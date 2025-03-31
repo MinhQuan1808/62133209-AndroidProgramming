a05cb6f54a34c16f392fd55d54f6a7d477e4f87c
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-31 08:57:43 +0700

    Cap nhat lan 2

---
+++ LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/Cau1Activity.java
@@ -0,0 +1,16 @@
+package ntu.quanndm.lamthukiemtra_lan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Cau1Activity extends AppCompatActivity {
+    Button btnC1, btnC2, btnC3
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cau1);
+    }
+}
