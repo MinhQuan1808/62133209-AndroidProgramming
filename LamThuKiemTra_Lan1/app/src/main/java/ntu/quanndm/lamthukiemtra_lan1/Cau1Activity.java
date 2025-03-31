116361a9347c7713be3830c4a1a4b614adc8675e
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-03-31 09:04:02 +0700

    Cap nhat lan 3

--- LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/Cau1Activity.java
+++ LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/Cau1Activity.java
@@ -2,15 +2,18 @@ package ntu.quanndm.lamthukiemtra_lan1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Cau1Activity extends AppCompatActivity {
-    Button btnC1, btnC2, btnC3
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cau1);
+
     }
 }
--- LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/MainActivity.java
+++ LamThuKiemTra_Lan1/app/src/main/java/ntu/quanndm/lamthukiemtra_lan1/MainActivity.java
@@ -2,13 +2,25 @@ package ntu.quanndm.lamthukiemtra_lan1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnC1, btnC2, btnC3, btnC4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnC1 = findViewById(R.id.btnC1);
+
+        btnC1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentC1 = new Intent(MainActivity.this, Cau1Activity.class);
+                startActivity(intentC1);
+            }
+        });
     }
 }
