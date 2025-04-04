1796015220dc90309084c5c10a939e62e0b36c67
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-04-04 14:08:21 +0700

    cap nhat

--- LamThuKiemTra_Lan2/app/src/main/java/ntu/quanndm/lamthukiemtra_lan2/Cau1Activity.java
+++ LamThuKiemTra_Lan2/app/src/main/java/ntu/quanndm/lamthukiemtra_lan2/Cau1Activity.java
@@ -3,12 +3,33 @@ package ntu.quanndm.lamthukiemtra_lan2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Cau1Activity extends AppCompatActivity {
-
+    EditText edtA, edtB, edtKQ;
+    Button btnTong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cau1);
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnTong = findViewById(R.id.btnTong);
+
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String so1 = edtA.getText().toString();
+                String so2 = edtB.getText().toString();
+                float soA = Float.parseFloat(so1);
+                float soB = Float.parseFloat(so2);
+                float tong = soA + soB;
+                String chuoiKQ = String.valueOf(tong);
+                edtKQ.setText(chuoiKQ);
+            }
+        });
     }
 }
--- LamThuKiemTra_Lan2/app/src/main/java/ntu/quanndm/lamthukiemtra_lan2/MainActivity.java
+++ LamThuKiemTra_Lan2/app/src/main/java/ntu/quanndm/lamthukiemtra_lan2/MainActivity.java
@@ -2,13 +2,35 @@ package ntu.quanndm.lamthukiemtra_lan2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnC1, btnC2, btnC3, btnC4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnC1 = findViewById(R.id.btnC1);
+        btnC2 = findViewById(R.id.btnC2);
+        btnC3 = findViewById(R.id.btnC3);
+        btnC4 = findViewById(R.id.btnC4);
+
+        btnC1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Cau1Activity.class);
+                startActivity(intent1);
+            }
+        });
+        btnC2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, Cau2Activity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
