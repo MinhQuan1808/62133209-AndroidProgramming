9bde67b90a006b686b9230b00717e340d0b8c197
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-04-04 16:26:01 +0700

    cap nhat

--- thigk2nguyendinhminhquan/app/src/main/java/ntu/quanndm/thigk2nguyendinhminhquan/Chucnang2Activity.java
+++ thigk2nguyendinhminhquan/app/src/main/java/ntu/quanndm/thigk2nguyendinhminhquan/Chucnang2Activity.java
@@ -3,12 +3,33 @@ package ntu.quanndm.thigk2nguyendinhminhquan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Chucnang2Activity extends AppCompatActivity {
-
+    EditText edtGK, edtCK, edtKQTB;
+    Button btnTB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chucnang2);
+        edtGK = findViewById(R.id.edtGK);
+        edtCK = findViewById(R.id.edtCK);
+        edtKQTB = findViewById(R.id.edtKQTB);
+        btnTB = findViewById(R.id.btnTB);
+
+        btnTB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String so1 = edtGK.getText().toString();
+                String so2 = edtCK.getText().toString();
+                float soA = Float.parseFloat(so1);
+                float soB = Float.parseFloat(so2);
+                float trungbinh = (soA + soB)/2;
+                String chuoiKQ = String.valueOf(trungbinh);
+                edtKQTB.setText(chuoiKQ);
+            }
+        });
     }
 }
--- thigk2nguyendinhminhquan/app/src/main/java/ntu/quanndm/thigk2nguyendinhminhquan/Chucnang3Activity.java
+++ thigk2nguyendinhminhquan/app/src/main/java/ntu/quanndm/thigk2nguyendinhminhquan/Chucnang3Activity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Chucnang3Activity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
