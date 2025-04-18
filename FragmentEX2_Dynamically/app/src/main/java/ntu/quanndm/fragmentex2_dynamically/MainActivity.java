19e80fe56587f3289ffef8d3934aeea2ae5ca92f
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-04-18 17:19:19 +0700

    Tao du an

---
+++ FragmentEX2_Dynamically/app/src/main/java/ntu/quanndm/fragmentex2_dynamically/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.quanndm.fragmentex2_dynamically;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
