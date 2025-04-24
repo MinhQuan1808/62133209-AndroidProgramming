438bd018785446144c073b8f4c8f0692d626e546
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-04-24 08:14:07 +0700

    hoan thanh, test ok

--- BottomNavigationView/app/src/main/java/ntu/quanndm/bottomnavigationview/MainActivity.java
+++ BottomNavigationView/app/src/main/java/ntu/quanndm/bottomnavigationview/MainActivity.java
@@ -2,6 +2,8 @@ package ntu.quanndm.bottomnavigationview;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -17,21 +19,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNav = findViewById(R.id.bot_nav);
+        loadFragment(new HomeFragment());
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment  fragmentduocchon = null;
                 int mnuItemDuocChon = item.getItemId();
                 if(mnuItemDuocChon == R.id.mnu_home){
+                    fragmentduocchon = new HomeFragment();
                     Toast.makeText(MainActivity.this, "Thay HOME", Toast.LENGTH_SHORT).show();
                 } else if(mnuItemDuocChon == R.id.mnu_search) {
+                    fragmentduocchon = new SearchFragment();
                     Toast.makeText(MainActivity.this, "Thay SEARCH", Toast.LENGTH_SHORT).show();
                 } else if (mnuItemDuocChon == R.id.mnu_profile) {
+                    fragmentduocchon = new ProfileFragment();
                     Toast.makeText(MainActivity.this, "Thay PROFILE", Toast.LENGTH_SHORT).show();
-                } else {
-                    return false;
                 }
-                return true;
+                if (fragmentduocchon != null) {
+                    loadFragment(fragmentduocchon);
+                    return true;
+                }
+                return false;
+
             }
         });
     }
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
 }
+
